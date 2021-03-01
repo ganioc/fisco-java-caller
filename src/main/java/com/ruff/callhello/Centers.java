c4a6d7c477cd00c354d7c56a52f4ffaac8566b3e
yango3 <o3.yang@hotmail.com>
2021-03-01 12:09:56 +0800

    1st commit

---
+++ src/main/java/com/ruff/callhello/Centers.java
@@ -0,0 +1,12 @@
+package com.ruff.callhello;
+
+import com.ruff.hello.Fizz;
+
+public class Centers {
+    public static void main(String[] args) {
+
+        System.out.println("hello centers!");
+        Fizz fb = new Fizz();
+        System.out.println(("Cetners get convert: " + fb.convert(100)));
+    }
+}
