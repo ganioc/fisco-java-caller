db50fc1379fc4bd77eff6a626894d6b09e8d6173
yango3 <o3.yang@hotmail.com>
2021-03-10 10:46:54 +0800

    Use posClient seems working

--- src/main/java/com/ruff/callhello/Centers.java
+++ src/main/java/com/ruff/callhello/Centers.java
@@ -1,12 +1,22 @@
 package com.ruff.callhello;
 
-import com.ruff.hello.Fizz;
+import com.ruff.hello.client.PosClient;
 
 public class Centers {
-    public static void main(String[] args) {
+    static String privateKey = "7a90e31dcd8e7b60dd89c9824cc96064c5e1538f94b7d7c1b3e5bf6a7a935cb8";
+    static String contractAddress = "0x33368951f3ac3581a612c6bce20e405d094257bd";
 
-        System.out.println("hello centers!");
-        Fizz fb = new Fizz();
-        System.out.println(("Centers get convert: " + fb.convert(100)));
+
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+
+        PosClient posClient = new PosClient(1, privateKey);
+
+        int rtn = posClient.getIndex(contractAddress);
+
+        System.out.println("index is: " + rtn);
+
+        posClient.stop();
+
     }
 }
